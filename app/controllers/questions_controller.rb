14773b96c55e19949559013b1abbcbe7da77c06f
Eduardo Thomazoni <eduardotps@gmail.com>
2022-08-15 14:55:31 -0300

    created stupid coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    # TODO: return coach answer to your_message
+    @answer = if params[:question] == 'I am going to work right now!'
+                'Great!'
+              elsif params[:question].include? '?'
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
